1928e651e86475fbf674a1ef4add6b24d5aa7a0c
Hakan ÖZER <hakanozer02@gmail.com>
2024-04-15 20:00:45 +0300

    Functions

--- Days-5/src/days/pkg5/Customer.java
+++ Days-5/src/days/pkg5/Customer.java
@@ -4,6 +4,57 @@ package days.pkg5;
 public class Customer {
     
     
+    // Değişkenler, Fonksiyonlar, Nesneler, vb.
+    
+    // Fonksiyonlar
+    // Olulşturma
+    // 1- Erişim belirteci -> public, protected, private
+    // 2- Geri dönüş değeri -> void, return type
+    // 3- Fonksiyon adı -> harf ile başlamalı, boşluk olmamalı, Türkçe karakter olmamalı
+    // 4- () -> Fonksiyon parametrelerinin yazdıldığı alan. birden fazla için "," ifadesi ile ayrım yapılır.
+    // 5- {} -> Body, Fonksiyon tetiklenmesi sırasında yapılacak işlemler(Kodlar) yazılır.
+    
+    
+    public void entry() {
+        System.out.println("entry call-"); 
+    }
+    
+    public boolean customerLogin(String username, String password ) {
+        if (username.equals("ali01") && password.equals("12345")) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public String customerName(int cid) {
+        String name = "";
+        if (cid == 10) {
+            name = "Ali Bilmem";
+        }else if (cid == 20) {
+            name = "Veli Bilirim";
+        }
+        
+        if ( nameControl(name) ) {
+            System.out.println("İçeriyor...");
+        }else {
+            System.out.println("İçermiyor...");
+        }
+        
+        return name;
+    } 
+    
+    
+    public boolean nameControl( String name ) {
+        String[] names = {"Ali", "Şeyma", "Ümit", "Çiğdem"};
+        for (String item : names) {
+            if (name.contains(item)) { 
+                return true;
+            }
+        }
+        return false;
+    }
+    
     
     
 }
--- Days-5/src/days/pkg5/Days5.java
+++ Days-5/src/days/pkg5/Days5.java
@@ -8,8 +8,28 @@ public class Days5 {
     public static void main(String[] args) {
         
         
+        // Nesne üretimi
+        Customer customer = new Customer();
         
+        customer.entry();
+        customer.entry();
+        customer.entry();
+        customer.entry();
+        
+        boolean status = customer.customerLogin("ali01", "12345");
+        System.out.println("ali01 Status : " + status);
+        
+        boolean status1 = customer.customerLogin("veli01", "123");
+        System.out.println("veli01 Status : " + status1);
+        
+        
+        String data = customer.customerName(20);
+        System.out.println(data);
+        
+        boolean status2 = customer.nameControl("Şeyma");
+        System.out.println("status2 :" + status2);
         
     }
     
+   
 }
